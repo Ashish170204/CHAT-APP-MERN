d7010d514db0ed572d86154f5f20573384c80595
Ashish Nautiyal <aashishnautiyal1702@gmail.com>
2026-01-10 19:13:02 +0530

    change in vite.config.js for strict frontend port

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server:{
-  //   port: 4002,
-  //   proxy:{
-  //     '/api': {
-  //       target: 'http://localhost:5022',
-  //       changeOrigin: true,
-  //   }
-  //  }
-  // }
-})
+  server: {
+    port: 5173,
+    strictPort: true, //  THIS is important
+  },
+});
